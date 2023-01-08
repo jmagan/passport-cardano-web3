de6f92059dc9c7f7e03b35bc9551d021198607d8
Juan Salvador Magán Valero <jmaganvalero@gmail.com>
2023-01-08 10:55:18 +0100

    feat: types definition

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,9 +1,19 @@
 import { Strategy as PassportStrategy } from 'passport-strategy';
 import { Request } from 'express';
+import { AuthenticateOptions } from 'passport'
+
+export type CardanoWeb3StrategyOptions = {
+    expirationTimeSpan: Number,
+    hostname: String
+}
+
+export type CardanoWeb3AuthenticateOptions = {
+    action: String
+} & AuthenticateOptions
 
 export declare class Strategy extends PassportStrategy {
-    constructor(verify: VerifyCallback);
-    authenticate(req: Request, options?: any): any;
+    constructor(options: CardanoWeb3StrategyOptions);
+    authenticate(req: Request, options?: CardanoWeb3AuthenticateOptions): any;
 }
 
 
